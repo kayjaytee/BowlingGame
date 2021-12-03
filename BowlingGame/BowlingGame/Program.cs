8aec41fae85101cd6cda0249a106ed7be1b800cd
Lilja01 <liljedahl.jonathan@gmail.com>
2021-12-03 15:00:19 +0100

    Grund variabler, grund metoder

--- BowlingGame/BowlingGame/Program.cs
+++ BowlingGame/BowlingGame/Program.cs
@@ -1,13 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BowlingGame
 {
 
     class Game
     {
+        //Ska vi lagra det i två lister och jämföra index? Eller
+        //List<int> scoreList = new List<int>(); 
+        //List<int> rollList = new List<int>();
+        Random rnd = new Random();
+
+        private int score = 0;
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
+        
+        
         private void Roll(int pins)
         {
-            Console.WriteLine("Hej");
+            score += pins;
+            rolls[currentRoll++] = pins;
+
+
+            //Roll roll = new Roll(pins);
         }
 
         private int Score()
@@ -21,6 +36,44 @@ namespace BowlingGame
 
         }
     }
+
+    class Frame
+    {
+        public int Pins;
+        public int Roll;
+
+        public Frame(int pins)
+        {
+            Pins = pins;
+            Roll++;
+            scoreEveryRound(pins);
+        }
+        private void scoreEveryRound(int pins)
+        {
+            if (pins < 10 || Roll < 2) //TenthFrame
+            {
+
+            }
+        }
+    }
+
+    /* (Eventeullt bortag)
+    class Roll
+    {
+        public int numberOfRolls;
+        public Roll(int pins)
+        {
+            if (pins == 10)
+            {
+                //return omgång slut
+            }
+            else
+            {
+
+            }
+        }
+    }
+    */
     class Program
     {
         static void Main(string[] args)
